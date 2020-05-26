11090252a04ab0a63b5eba010ff36f619bbb3999
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-05-26 09:29:03 -0600

    added sort drop down list

--- src/App.js
+++ src/App.js
@@ -7,31 +7,36 @@ import "./App.css";
 
 const App = () => {
   const [employees, setEmployees] = useState(employeeList);
-  
+
   const [searchState, setSearchState] = useState("");
   const { searchTerm } = searchState;
 
   const [sortState, setSortState] = useState([]);
-  
-  const handleInputChange = e => {
-    const { name, value} = e.target;
-    setSearchState({...searchState, [name]: value});
-    
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setSearchState({ ...searchState, [name]: value });
   };
-  const handleFormSubmit = e => {
+  const handleFormSubmit = (e) => {
     e.preventDefault();
-    const findEmployee = employees.filter(employee => employee.name === searchTerm);
+    const findEmployee = employees.filter(
+      (employee) => employee.name === searchTerm
+    );
     setEmployees(findEmployee);
   };
-  
-  const handleSort = type => {
+
+  const handleSort = (type) => {
     const types = {
-      name: 'name',
-      phone: 'phone',
-      email: 'email',
+      name: "name",
+      phone: "phone",
+      email: "email",
     };
     const sortProperty = types[type];
-    const sorted = [...employees].sort((a, b) => b[sortProperty] - a[sortProperty]);
+ 
+    alert(sortProperty);
+    const sorted = [...employees].sort(
+      (a, b) => a[sortProperty] > b[sortProperty] ? 1 : -1
+    );
     console.log(sorted);
     setEmployees(sorted);
   };
@@ -50,25 +55,21 @@ const App = () => {
             id="search"
             onChange={handleInputChange}
           />
-          <button
-            className="btn btn-primary mt-3"
-          >
-            Search
-          </button>
+          <button className="btn btn-primary mt-3">Search</button>
         </div>
       </form>
-      <div >
-         <h6> sort by:</h6>
-    <select onChange={(e) => handleSort(e.target.value)}>
-   <option value="name">Name</option>
-   <option value="phone">Phone</option>
-   <option value="email">Email</option>
- </select>
+      <div>
+        <h6> sort by:</h6>
+        <select onChange={(e) => handleSort(e.target.value)}>
+          <option value="name">Name</option>
+          <option value="phone">Phone</option>
+          <option value="email">Email</option>
+        </select>
       </div>
       <table className="table table-striped">
         <thead>
           <tr>
-            <th scope="col" >Name </th>
+            <th scope="col">Name </th>
             <th scope="col">Phone</th>
             <th scope="col">Email</th>
           </tr>
